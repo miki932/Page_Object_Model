6252fbaf648303676aa898a9f34c52fd6304bbf6
michaelziony <michael.ziony@armis.com>
2022-03-28 21:54:10 +0300

    some improvment

--- Config/config.py
+++ Config/config.py
@@ -1,11 +1,7 @@
 class Test_Data:
-    # We should `gitignore` this file,
-    # because we have secret information here (password)...
-    CHROME_PATH = "/Users/Michael/Download/chromedriver"
-    FIREFOX_PATH = "/Users/Michael/Download/geckodriver"
+
     BASE_URL = "https://www.saucedemo.com/"
     HOME_PAGE_URL = "https://www.saucedemo.com/inventory.html"
     USER_NAME = "standard_user"
     INVALID_USER_NAME = "bad_user"
     PASSWORD = "secret_sauce"
-    LOGIN_PAGE_TITLE = "Swag Labs"
