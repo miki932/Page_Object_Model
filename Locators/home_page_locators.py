790815d3da1e479072d8fc6f40eaadb56344b732
Michael Tsioni <mtsioni@akamai.com>
2022-10-02 18:53:03 +0300

    locators refactor

--- Locators/home_page_locators.py
+++ Locators/home_page_locators.py
@@ -1,49 +1,47 @@
 from selenium.webdriver.common.by import By
 
-# Object Repository:
-SHOPPING_CART = (By.ID, "shopping_cart_container")
-SHOPPING_CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
-# SHOPPING_CART_BADGE = (
-#     By.XPATH,
-#     '//*[@id="shopping_cart_container"]//*[@class="shopping_cart_link"]//*[@class="shopping_cart_badge"]',
-# )
-BURGER_MENU = (By.ID, "react-burger-menu-btn")
-SORT_BTN = (By.CLASS_NAME, "active_option")
-BACK_TO_PRODUCT = (By.ID, "back-to-products")
-ADD_TO_CART = (By.ID, "add-to-cart-sauce-labs-backpack")
-REMOVE_FROM_CART = (By.ID, "remove-sauce-labs-backpack")
 
-ITEM_FOR_SALE_1 = (
-    By.XPATH,
-    '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_4_title_link"]',
-)
-ITEM_FOR_SALE_2 = (
-    By.XPATH,
-    '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_0_title_link"]',
-)
-ITEM_FOR_SALE_3 = (
-    By.XPATH,
-    '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_1_title_link"]',
-)
-ITEM_FOR_SALE_4 = (
-    By.XPATH,
-    '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_2_title_link"]',
-)
-ITEM_FOR_SALE_5 = (
-    By.XPATH,
-    '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_3_title_link"]',
-)
+class HomePageLocators:
+    # Object Repository:
+    SHOPPING_CART = (By.ID, "shopping_cart_container")
+    SHOPPING_CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
+    BURGER_MENU = (By.ID, "react-burger-menu-btn")
+    SORT_BTN = (By.CLASS_NAME, "active_option")
+    BACK_TO_PRODUCT = (By.ID, "back-to-products")
+    ADD_TO_CART = (By.ID, "add-to-cart-sauce-labs-backpack")
+    REMOVE_FROM_CART = (By.ID, "remove-sauce-labs-backpack")
 
-# Footer
-TWITTER_LOGO = (By.XPATH, '//*[@id="page_wrapper"]/footer/ul/li[1]/a')
-FACEBOOK_LOGO = (By.XPATH, '//*[@id="page_wrapper"]/footer/ul/li[2]/a')
-LINKEDIN_LOGO = (By.XPATH, '//*[@id="page_wrapper"]/footer/ul/li[3]/a')
+    ITEM_FOR_SALE_1 = (
+        By.XPATH,
+        '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_4_title_link"]',
+    )
+    ITEM_FOR_SALE_2 = (
+        By.XPATH,
+        '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_0_title_link"]',
+    )
+    ITEM_FOR_SALE_3 = (
+        By.XPATH,
+        '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_1_title_link"]',
+    )
+    ITEM_FOR_SALE_4 = (
+        By.XPATH,
+        '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_2_title_link"]',
+    )
+    ITEM_FOR_SALE_5 = (
+        By.XPATH,
+        '//*[@id="inventory_container"]//*[@class="inventory_list"]//*[@class="inventory_item"]//*[@id="item_3_title_link"]',
+    )
 
-TWITTER_URL = "https://twitter.com/saucelabs"
-FACEBOOK_URL = "https://www.facebook.com/saucelabs"
-CART_PAGE_URL = "https://www.saucedemo.com/cart.html"
+    # Footer
+    TWITTER_LOGO = (By.XPATH, '//*[@id="page_wrapper"]/footer/ul/li[1]/a')
+    FACEBOOK_LOGO = (By.XPATH, '//*[@id="page_wrapper"]/footer/ul/li[2]/a')
+    LINKEDIN_LOGO = (By.XPATH, '//*[@id="page_wrapper"]/footer/ul/li[3]/a')
 
-# Burger Button
-ABOUT_PAGE = (By.ID, "about_sidebar_link")
-ABOUT_PAGE_URL = "https://saucelabs.com/"
-LOGOUT_BTN = (By.ID, "logout_sidebar_link")
+    TWITTER_URL = "https://twitter.com/saucelabs"
+    FACEBOOK_URL = "https://www.facebook.com/saucelabs"
+    CART_PAGE_URL = "https://www.saucedemo.com/cart.html"
+
+    # Burger Button
+    ABOUT_PAGE = (By.ID, "about_sidebar_link")
+    ABOUT_PAGE_URL = "https://saucelabs.com/"
+    LOGOUT_BTN = (By.ID, "logout_sidebar_link")
