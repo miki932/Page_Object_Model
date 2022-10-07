5ead3443f7dcfb5a90057d5649384315f9f5b6be
Michael Tsioni <mtsioni@akamai.com>
2022-10-07 20:01:26 +0300

    add testData class

--- Tests/test_login_page.py
+++ Tests/test_login_page.py
@@ -1,25 +1,27 @@
 from Pages.base_page import BasePage
 from Pages.login_page import LoginPage
 from Locators.login_page_locators import LoginLocators
-import config
+from config import TestData
 
 
 class TestLogin(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.login_page = LoginPage(self.driver)
-        self.driver.get(config.BASE_URL)
+        self.driver.get(TestData.BASE_URL)
 
     def test_login_page_title(self):
         title = self.login_page.get_login_title(LoginLocators.LOGIN_PAGE_TITLE)
         assert title == LoginLocators.LOGIN_PAGE_TITLE
 
     def test_login(self):
-        self.login_page.do_login(config.USER_NAME, config.PASSWORD)
+        self.login_page.do_login(TestData.USER_NAME, TestData.PASSWORD)
         current_url = self.login_page.get_url()
-        assert current_url == config.HOME_PAGE_URL
+        assert current_url == TestData.HOME_PAGE_URL
 
     def test_invalid_login(self):
-        self.login_page.invalid_login(config.INVALID_USER_NAME, config.INVALID_PASSWORD)
+        self.login_page.invalid_login(
+            TestData.INVALID_USER_NAME, TestData.INVALID_PASSWORD
+        )
         err_msg = self.login_page.is_visible(LoginLocators.ERROR_MESSAGE)
         assert err_msg
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-BASE_URL = "https://www.saucedemo.com/"
-HOME_PAGE_URL = "https://www.saucedemo.com/inventory.html"
-USER_NAME = "standard_user"
-INVALID_USER_NAME = "bad_user"
-PASSWORD = "secret_sauce"
-INVALID_PASSWORD = "12345"
-TIMEOUT = 10
+class TestData:
+    BASE_URL = "https://www.saucedemo.com/"
+    HOME_PAGE_URL = "https://www.saucedemo.com/inventory.html"
+    USER_NAME = "standard_user"
+    INVALID_USER_NAME = "bad_user"
+    PASSWORD = "secret_sauce"
+    INVALID_PASSWORD = "12345"
+    TIMEOUT = 10
