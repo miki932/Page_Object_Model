07246024b70c3947494d2442fd30740025d2718b
Michael Tsioni <mtsioni@akamai.com>
2022-10-07 18:48:05 +0300

    import environ instead all os library

---
+++ .env.py
@@ -0,0 +1,4 @@
+from os import environ
+
+SAUCE_USERNAME = environ.get("SAUCE_USERNAME")
+SAUCE_ACCESS_KEY = environ.get("SAUCE_ACCESS_KEY")
--- Tests_Suite/.env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-SAUCE_USERNAME = os.environ.get("SAUCE_USERNAME")
-SAUCE_ACCESS_KEY = os.environ.get("SAUCE_ACCESS_KEY")
