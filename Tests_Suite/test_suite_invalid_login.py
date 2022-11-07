89ead1b85b664baff75570d722d4f5b989d6aa07
Michael Tsioni <mtsioni@akamai.com>
2022-11-07 18:07:28 +0200

    test_shopping_badge_cart

--- Tests_Suite/test_suite_invalid_login.py
+++ Tests_Suite/test_suite_invalid_login.py
@@ -1,7 +1,6 @@
 from Tests.test_login_page import TestLogin
 
 
-class TestInvalidLogin:
-    def invalid_login(self, init_driver):
-        test_login = TestLogin(init_driver)
-        test_login.test_invalid_login()
+def invalid_login(init_driver):
+    test_login = TestLogin(init_driver)
+    test_login.test_invalid_login()
