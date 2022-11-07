7481652b220bf31fb6b3cf73df4639d6bcf52274
Michael Tsioni <mtsioni@akamai.com>
2022-11-07 17:10:30 +0200

    test_shopping_badge_cart

--- Locators/cart_page_locators.py
+++ Locators/cart_page_locators.py
@@ -8,6 +8,7 @@ class CartPageLocators:
     REMOVE_BTN = (By.ID, "remove-sauce-labs-backpack")
     ITEM_0_TITLE_LINK = (
         By.ID,
-        '//*[@class="cart_list"]//*[@class="cart_item"]//*[@class="cart_item_label"]//*[@id="item_0_title_link"]',
+        '//*[@class="cart_list"]//*[@class="cart_item"]'
+        '//*[@class="cart_item_label"]//*[@id="item_0_title_link"]',
     )
     CART_PAGE_URL = "https://www.saucedemo.com/cart.html"
